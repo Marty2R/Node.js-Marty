2755c07f9a84b639447acf5f12b2b98f4982e2be
Marty Rabatel <marty.rabatel2@gmail.com>
2022-12-19 13:48:32 +0100

    Ajout de la fonction number min

---
+++ app.js
@@ -0,0 +1,18 @@
+// Ecrire une fonction qui prend un tableau de nombre en paramètre et qui retourne le plus grand nombre du tableau :
+
+const array = [134, 47, 8, 62, 7, 28];
+
+function small (myArray) {
+
+    let small = 1000;
+
+    for (let i = 0; i < myArray.length; i++) {
+
+        myArray[i] < small ? small = myArray[i] : console.log(small);
+        
+    }
+
+    console.log(small);
+    return small;
+    
+} small (array);
